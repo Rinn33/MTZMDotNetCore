c1575428c33003e5066594bf627f217fc53da892
Rinn33 <rinn88142@gmail.com>
2024-04-21 17:32:07 +0630

    Add project files.

---
+++ MTZMDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MTZMDotNetCore.ConsoleApp;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello World");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("Author","Title", "Content");
+//adoDotNetExample.Update(1, "Author", "Title", "Content");
+//adoDotNetExample.Delete(11);
+adoDotNetExample.Edit(11);
+adoDotNetExample.Edit(9);
+Console.ReadKey();
